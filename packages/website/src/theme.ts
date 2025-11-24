9a06e4f9710352cc749af329301fbdae6369f10e
Milan Patel <32933108+thempatel@users.noreply.github.com>
2025-11-24 14:08:38 -0500

    refactor: drop old ts backed blog format

--- packages/website/src/theme.ts
+++
@@ -1,9 +0,0 @@
-import { theme as primerTheme } from '@primer/components';
-
-const baseTheme = {
-  ...primerTheme,
-};
-
-baseTheme.colors.black = '#0d1117';
-
-export const theme = baseTheme;
--- tools/deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-BRANCH="gh-pages"
-git branch -D ${BRANCH}
-git subtree split --prefix dist -b ${BRANCH}
-git push --force origin ${BRANCH}
